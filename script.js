365fe76fab38ffe1107c755a4244a1f65f395536
alexpat-86 <alex.paterson860@gmail.com>
2022-11-19 22:32:02 +0000

    some testing

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const player = document.getElementById("player");
 const obstacle = document.getElementById("obstacle");
+const playspace = document.getElementById("playspace");
 
-function jump(){
+const rotor = document.getElementById("rotator");
+
+function jump() {
     if(player.classList == "animate"){return}
     player.classList.add("animate");
     setTimeout(function(){
         player.classList.remove("animate");
     },300);
 }
+
+
+const onMouseMove = (e) =>{
+    player.style.left = e.pageX + 'px';
+    player.style.top = e.pageY + 'px';
+}
+  
+document.addEventListener('mousemove', onMouseMove);
+ 
+const bullet = document.getElementById("bullet");
+
+document.body.onkeyup = function(e) {
+    if (e.key == " " ||
+        e.code == "Space" ||      
+        e.keyCode == 32) {
+            bullet.style.color("red");
+    }
+}
+  
+
